72bdc9e9d6ba2e96a2fd308716ee3e75e5f200b6
beautymonkey1437 <780311353@qq.com>
2025-10-30 20:40:23 +0800

    202510302040

--- OnComputerOfSchool/20251030/4.c
+++ OnComputerOfSchool/20251030/4.c
@@ -11,3 +11,78 @@
 
 （4）将日期和时间单独一行写入“result.txt”文件中，
 紧接着把排序后的数组写在日期和时间的下一行。*/
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    FILE *fpi, *fpo;
+    time_t seconds;
+    char days_mons[2][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+                             {32, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}},
+         leap = 0;
+    int year, month, day, hour, minute, second;
+    float a[100]; // float数组
+    int n;
+    int i, j, k, l, m;
+    float t;
+    printf("有几个数(1~100):");
+    scanf("%d", &n);
+    fpi = fopen("data.txt", "r");
+    if (fpi == NULL)
+    {
+        fpi = fopen("data.txt", "w+");
+        printf("输入%d个数:", n);
+        for (i = 0; i < n; i++)
+        {
+            scanf("%f", &a[i]);
+            fprintf(fpi, "%f ", a[i]);
+        }
+        rewind(fpi);
+    }
+    fpo = fopen("result.txt", "a");
+    for (i = 0; i < n; i++)
+        fscanf(fpi, "%f", &a[i]);
+    fclose(fpi);
+    seconds = time(NULL) + 8 * 60 * 60;
+    second = seconds % 60;
+    seconds /= 60;
+    minute = seconds % 60;
+    seconds /= 60;
+    hour = seconds % 24;
+    seconds /= 24;
+    year = 1970;
+    leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    while ((leap && seconds > 365) || (!leap && seconds > 364))
+    {
+        if (leap)
+            seconds -= 366;
+        else
+            seconds -= 365;
+        year++;
+        leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+    month = 1;
+    for (i = 0; seconds >= days_mons[leap][i]; i++, month++)
+        seconds -= days_mons[leap][i];
+    day = seconds + 1;
+    fprintf(fpo, "%d-%02d-%02d,%02d:%02d:%02d\n", year, month, day, hour, minute, second);
+    // 排序
+    for (k = 0; k < n - 1; k++)
+    {
+        for (l = k + 1; l < n; l++)
+        {
+            if (a[k] > a[l]) // 升序
+            {
+                t = a[k];
+                a[k] = a[l];
+                a[l] = t;
+            }
+        }
+    }
+    for (m = 0; m < n; m++)
+        fprintf(fpo, "%f ", a[m]);
+    fprintf(fpo, "\n");
+    fclose(fpo);
+    return 0;
+}
